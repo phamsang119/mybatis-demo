66b45316351c756718f7c8a513ce004999f58f99
mainguyenngocanh1411 <mainguyenngocanh1411@gmail.com>
2019-11-14 11:39:00 +0700

    [Code] Add transactional

--- backend/src/main/java/metanet/book/service/BookServiceImpl.java
+++ backend/src/main/java/metanet/book/service/BookServiceImpl.java
@@ -5,11 +5,13 @@ import metanet.book.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
 import java.util.Random;
 
 @Service
+@Transactional
 public class BookServiceImpl implements BookService {
 
     @Autowired
