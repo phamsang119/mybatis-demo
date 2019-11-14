763768cd922a98e4d1c843e6eb3fcbc6528b696d
Sang Pham <sang.pham@metanet.co.kr>
2019-11-14 15:52:59 +0700

    update infinity scroll

--- backend/src/main/java/metanet/book/repository/BookRepository.java
+++ backend/src/main/java/metanet/book/repository/BookRepository.java
@@ -10,20 +10,20 @@ import java.util.List;
 @Repository
 public interface BookRepository {
 
-//    @Select("SELECT * from books LIMIT #{offset}, #{limit} ")
+    @Select("SELECT * from books LIMIT #{offset}, #{limit} ")
     List<Book> getBooks(@Param("offset") int offset, @Param("limit") int limit);
 
-    //@Select("SELECT * from books where id = #{id} ")
+    @Select("SELECT * from books where id = #{id} ")
     Book getBookById(long id);
 
-    //@Insert("INSERT INTO books (id, author, bookName,description,price,published_date, category) VALUES (#{id}, #{author}, #{bookName},#{description},#{price},#{publishedDate},#{category})")
+    @Insert("INSERT INTO books (id, author, bookName,description,price,published_date, category) VALUES (#{id}, #{author}, #{bookName},#{description},#{price},#{publishedDate},#{category})")
     // Sets the object id to the id generated in database
-    //@Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
+    @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
     int insertBook(Book book);
 
-   //@Update("UPDATE books SET author=#{author},bookName=#{bookName}, description=#{description}, price=#{price}, published_date=#{publishedDate}, category=#{category} where id=#{id}")
+   @Update("UPDATE books SET author=#{author},bookName=#{bookName}, description=#{description}, price=#{price}, published_date=#{publishedDate}, category=#{category} where id=#{id}")
     int updateBook(Book book);
 
-   //@Update("DELETE FROM books where id=#{id}")
+   @Update("DELETE FROM books where id=#{id}")
     void deleteBook(long id);
 }
