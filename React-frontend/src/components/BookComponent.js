aa6db183004c7c2c9937fdb26f232e17caccd98a
Sang Pham <sang.pham@metanet.co.kr>
2019-11-15 17:43:13 +0700

    updat front end

--- React-frontend/src/components/BookComponent.js
+++ React-frontend/src/components/BookComponent.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import {bookService} from '../services/BookService';
 import {findDOMNode} from 'react-dom';
-import {FormControl, InputLabel, Input, Button, makeStyles} from '@material-ui/core';
+import {FormControl, InputLabel, Input, Button} from '@material-ui/core';
 import  { Editors }  from 'react-data-grid-addons';
 import ReactDataGrid from 'react-data-grid';
-
-const { DropDownEditor } = Editors;
-
-const Categories = [
-    { id: "it", value: "IT" },
-    { id: "cooking", value: "Cooking" },
-    { id: "teen", value: "Teen" }
-];
-const categoryTypeEditor = <DropDownEditor options={Categories} />;
-
-const columns = [
-    {key: 'id', name: 'ID'},
-    {key: 'bookName', name: 'Book Name', editable: true},
-    {key: 'author', name: 'Author', editable: true},
-    {key: 'description', name: 'Description', editable: true},
-    {key: 'price', name: 'Price', editable: true},
-    {key: 'category', name: 'Category', editor: categoryTypeEditor}];
 
 
 class BookComponent extends React.Component {
@@ -31,13 +14,13 @@ class BookComponent extends React.Component {
             editedBooks: [],
             localBooks: [],
             offSet: 1,
-            limit: 20
+            limit: 20,
+            total: 0
         };
     }
-
     componentDidMount() {
         bookService.get("books", {page: this.state.offSet, limit: this.state.limit}).then((result) => {
-            this.setState({localBooks: result.data.books});
+            this.setState({localBooks: result.data.books, total: result.data.count});
         });
         this.bookTableDom = findDOMNode(this).querySelector('.react-grid-Canvas');
         this.bookTableDom.addEventListener('scroll', this.scrollListener);
@@ -70,19 +53,57 @@ class BookComponent extends React.Component {
             bookService.get("books", {page: this.state.offSet + 1, limit: this.state.limit}).then((result) => {
                 this.setState({
                     localBooks: [...this.state.localBooks, ...result.data.books],
-                    offSet: this.state.offSet + 1
+                    offSet: this.state.offSet + 1,
+                    total: result.data.count
                 });
             })
         }
     };
 
     saveBook = () => {
-        bookService.put("books/updateList", {listBooks: this.state.editedBooks}).then(() => {
-            this.setState({editedBooks: []});
+        bookService.post("books/updateList", {listBooks: this.state.editedBooks}).then((response) => {
+            if(response && response.status === 200) {
+                alert("Save successfully");
+                this.setState({editedBooks: []});
+            } else {
+                alert("Save unsuccessfully");
+                throw response;
+            }
         })
     };
 
+    sortRows = (initialRows, sortColumn, sortDirection) => {
+        const comparer = (a, b) => {
+            if (sortDirection === "ASC") {
+                return a[sortColumn] > b[sortColumn] ? 1 : -1;
+            } else if (sortDirection === "DESC") {
+                return a[sortColumn] < b[sortColumn] ? 1 : -1;
+            }
+        };
+        if(sortDirection !== "NONE") {
+            const books = this.state.localBooks.slice();
+            this.setState({localBooks: books.sort(comparer)})
+        }
+    };
+
     render() {
+        const { DropDownEditor } = Editors;
+
+        const Categories = [
+            { id: "it", value: "IT" },
+            { id: "cooking", value: "Cooking" },
+            { id: "teen", value: "Teen" }
+        ];
+        const categoryTypeEditor = <DropDownEditor options={Categories} />;
+
+        const columns = [
+            {key: 'id', name: 'ID', sortable : true},
+            {key: 'bookName', name: 'Book Name', editable: true, sortable : true},
+            {key: 'author', name: 'Author', editable: true, sortable : true},
+            {key: 'description', name: 'Description', editable: true, sortable : true},
+            {key: 'price', name: 'Price', editable: true, sortable : true},
+            {key: 'category', name: 'Category', editor: categoryTypeEditor, sortable : true}];
+
         return (
             <div style={{maxWidth: "90%", margin: "auto"}}>
                 <div className="searchForm" style={{float: "left",  marginBottom: "15px"}}>
@@ -93,7 +114,7 @@ class BookComponent extends React.Component {
                 </div>
                 <div className="tableButton" style={{float:"right", marginTop: "15px"}}>
 
-                    <Button variant="outlined" onClick={this.saveBook}>
+                    <Button variant="outlined" onClick={this.saveBook} style={{marginRight: "5px"}}>
                         Save
                     </Button>
                     <Button variant="outlined">
@@ -106,8 +127,14 @@ class BookComponent extends React.Component {
                     rowsCount={this.state.localBooks.length}
                     enableCellSelect={true}
                     onGridRowsUpdated={this.onGridRowsUpdated}
-                    minHeight={600}
+                    minHeight={500}
+                    onGridSort={(sortColumn, sortDirection) =>
+                        this.sortRows(this.state.localBooks, sortColumn, sortDirection)
+                    }
                 />
+                <div  style={{float: "right",  marginBottom: "15px"}}>
+                    Showing {this.state.localBooks.length} of {this.state.total} item
+                </div>
             </div>
         );
     };
